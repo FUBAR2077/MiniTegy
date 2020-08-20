07b5a21a82846f52a46f4f35e81df733e1cd865e
GKiernozek <gkiernozek@gmail.com>
2020-08-20 08:23:38 +0200

    init project files

---
+++ Assets/_Project/Scripts/Builder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    public BuildingType currentBuildingType;
+
+    public Extractor extractorPrefab;
+
+    private void Awake()
+    {
+        currentBuildingType = BuildingType.None;
+    }
+
+    public void SetNoneBuildingMode()
+    {
+        SetBuilderMode(BuildingType.None);
+    }
+
+    public void SetExtractorBuildingMode()
+    {
+        SetBuilderMode(BuildingType.Extractor);
+    }
+
+    public void SetBuilderMode(BuildingType buildingType)
+    {
+        currentBuildingType = buildingType;
+    }
+
+    public void Build(Vector2 position)
+    {
+        if (currentBuildingType == BuildingType.None) return;
+
+        switch (currentBuildingType)
+        {
+            case BuildingType.Extractor:
+                BuildExtractor(position);
+                break;
+        }
+    }
+
+    private void BuildExtractor(Vector2 position)
+    {
+        Instantiate(extractorPrefab, position, Quaternion.identity);
+    }
+}
---
+++ Assets/_Project/Scripts/InputLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputLogic : MonoBehaviour
+{
+    public Builder builder;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            builder.Build(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+}
